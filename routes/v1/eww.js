850595c7f71eb6f743989758e974ba22bdb3f048
tutyamxx <tutygoo@gmail.com>
2020-09-13 19:14:54 +0100

    Added missing parentheses :angry:

--- routes/v1/eww.js
+++ routes/v1/eww.js
@@ -25,7 +25,7 @@ router.get("/eww", (req, res, next) =>
     let FontSize = (szName.length >= 20) ? 14 : 20;
 
     gm(join(__dirname, "../../public/images/eww/eww.png"))
-    .font(join(__dirname, "../../public/fonts/Helvetica.ttf", FontSize)
+    .font(join(__dirname, "../../public/fonts/Helvetica.ttf", FontSize))
     .fill("#111111")
     .draw(["rotate -55 text -430, 480 '" + szName.trim() + "'"])
     .toBuffer("eww.png", (err, buffer) =>
